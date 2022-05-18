9cd08881c4d838d7314410362eb837cdab5ef7b5
Matias-Nicolas.Vargas <matias-nicolas.vargas@endava.com>
2022-05-18 12:44:31 -0300

    simple work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::env;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    let query = &arguments[1];
+    let file = &arguments[2];
+    println!("Query= \"{}\" \nFile=\"{}\"", query, file);
+    let file_content = fs::read_to_string(file).expect("Something went wrong reading the file");
+    let indices = file_content.match_indices(query);
+    let mut aux_index = 0;
+    for elem in indices {
+        print!("{}",&file_content[aux_index..elem.0]);
+        print!("{}",elem.1.bold().red());
+        aux_index = elem.0+elem.1.len();
+    }
+    print!("{}",&file_content[aux_index..]);
 }
