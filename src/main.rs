72120a2adfd83c929449677a2cc12bf0d44b296b
Matias-Nicolas.Vargas <matias-nicolas.vargas@endava.com>
2022-05-19 17:18:25 -0300

    this up

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use std::env;
 use colored::*;
 
 fn main() {
-    let arguments: Vec<String> = env::args().collect();
-    let query = &arguments[1];
-    let file = &arguments[2];
-    println!("Query= \"{}\" \nFile=\"{}\"", query, file);
-    let file_content = fs::read_to_string(file).expect("Something went wrong reading the file");
-    let indices = file_content.match_indices(query);
+    let arguments = Config::new(&env::args().collect::<Vec<String>>());
+    println!("Query= \"{}\" \nFile=\"{}\"", arguments.query, arguments.file);
+    let file_content = fs::read_to_string(arguments.file).expect("Something went wrong reading the file");
+    let indices = file_content.match_indices(&arguments.query);
     let mut aux_index = 0;
     for elem in indices {
         print!("{}",&file_content[aux_index..elem.0]);
@@ -16,4 +14,22 @@ fn main() {
         aux_index = elem.0+elem.1.len();
     }
     print!("{}",&file_content[aux_index..]);
+}
+
+struct Config {
+    query: String,
+    file: String
+}
+
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file = args[2].clone();
+
+        Ok(Config { query, file })
+    }
 }
